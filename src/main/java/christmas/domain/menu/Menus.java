309d7c0932d92a40590809560f9c72cb77bc7832
Suihub <qkdcrt@gmail.com>
2023-12-12 22:10:12 +0900

    <feat(Menus)>: "일급 컬렉션 Menus 생성" - Map으로 메뉴와 개수를 관리하는 Menus 생성

---
+++ src/main/java/christmas/domain/menu/Menus.java
@@ -0,0 +1,11 @@
+package christmas.domain.menu;
+
+import java.util.Map;
+
+public class Menus {
+    private final Map<Menu, Integer> menus;
+
+    public Menus(Map<Menu, Integer> menus) {
+        this.menus = menus;
+    }
+}
