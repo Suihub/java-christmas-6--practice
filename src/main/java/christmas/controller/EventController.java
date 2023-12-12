f23a47a10056622f92b79e37ded1adce19ae9ccf
Suihub <qkdcrt@gmail.com>
2023-12-12 21:06:06 +0900

    <feat>: "Controller 및 View 생성" - Controller, View 생성

---
+++ src/main/java/christmas/controller/EventController.java
@@ -0,0 +1,18 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class EventController {
+    private final InputView input;
+    private final OutputView output;
+
+    public EventController(InputView input, OutputView output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public void start() {
+
+    }
+}
