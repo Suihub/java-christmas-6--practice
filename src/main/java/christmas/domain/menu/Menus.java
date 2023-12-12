28276959697c509c0b2089d5b6beb0aaaed05314
Suihub <qkdcrt@gmail.com>
2023-12-12 23:03:32 +0900

    <feat(Menus)>: "메뉴 관련 검증 기능 구현" - 음료수만 주문했을 시, 20개가 넘어갈 시, 존재하지 않는 메뉴일시를 검증 - 위에 따른 기능 목록 수정

--- src/main/java/christmas/domain/menu/Menus.java
+++ src/main/java/christmas/domain/menu/Menus.java
@@ -6,6 +6,7 @@ import static java.util.stream.Collectors.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class Menus {
     private final Map<Menu, Integer> menus;
@@ -15,5 +16,38 @@ public class Menus {
         this.menus = orders.stream()
                 .map(order -> order.split("-"))
                 .collect(toMap(list -> orderMenu(list[0]), list -> Integer.parseInt(list[1])));
+        notExistMenuValidate();
+        onlyDrinkValidate();
+        menuNumMaxValidate();
+    }
+
+    private void notExistMenuValidate() {
+        Optional<Menu> result = menus.keySet().stream()
+                .filter(menu -> menu.getMenuCode() == 0)
+                .findAny();
+
+        if (result.isPresent()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void onlyDrinkValidate() {
+        long result = menus.keySet().stream()
+                .filter(menu -> menu.getMenuCode() == 4)
+                .count();
+
+        if (result == menus.size()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void menuNumMaxValidate() {
+        int result = menus.values().stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        if (result > 20) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 }
